89f76fe2e2685445c542eb91f861d03a53382bb4
LeCitronVert <youcefphnixlord@gmail.com>
2019-06-09 21:16:37 +0200

    Début du travail

---
+++ site/index.php
@@ -0,0 +1,71 @@
+<?php
+
+?>
+
+<!DOCTYPE HTML>
+<head>
+    <title>NatsuCon 2K19 !</title>
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/index.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="effetpara"></div>
+
+    <header>
+        <div class="selecteur">
+            Accueil
+        </div>
+        
+        <div class="selecteur">
+            Actus
+        </div>
+        
+        <div class="selecteur">
+            Exposants
+        </div>
+        
+        <div class="selecteur">
+            Animations
+        </div>
+        
+        <div class="selecteur">
+            FAQ/Contact
+        </div>
+    </header>
+    
+    
+    <div id="presentation">
+        
+        <span id="tagline">
+            NatsuCon revient en 2019 !
+        </span>
+        
+        <iframe src="https://www.youtube.com/embed/6-9w5e6SfiE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        
+        <span id="joinhim">
+            Alors, rejoindrez-vous Natsu cet été ?
+        </span>
+        
+    </div>
+    
+
+    <div id="langueass">
+        <img src="ressources/geoffrey_en.png" id="logo_en">
+        
+        <div id="travelers">
+            <span id="travelling">International attendees :</span>
+            
+            <span id="needhelp"></span>
+        
+        
+        </div>
+    </div>
+    
+    <footer>
+    
+    </footer>
+    
+    
+
+</body>
